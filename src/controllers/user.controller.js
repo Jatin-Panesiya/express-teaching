6f7c1d1dc78c160289b2d940c7a7fb90e3f4c0ed
Jatin <jatinpanesiya2003@gmail.com>
2025-08-19 16:27:53 +0530

    crud complete

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -11,9 +11,51 @@ export const getUsers = async (req, res) => {
 
 export const createUser = async (req, res) => {
     try {
-        await user.create({ username: "Paras", password: "456" })
+        const data = req.body;
+        if (!data.username || !data.password) {
+            return res.status(400).json({ message: "username and password are required!" })
+        }
+        await user.create(data)
         res.status(201).json({ message: "User created !" })
     } catch (error) {
         res.status(500).json({ message: error.message || "Error while creating user" })
     }
 }
+
+export const deleteUser = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        if (!userId) {
+            res.status(400).json({ message: "User not found! please provide user id" })
+            return
+        }
+        // await user.deleteMany()
+        await user.deleteOne({ _id: userId })
+        res.status(201).json({ message: "User deleted !" })
+    } catch (error) {
+        res.status(500).json({ message: error.message || "Error while creating user" })
+    }
+}
+
+export const updateUser = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const data = req.body;
+
+        if (!data) {
+            res.status(400).json({ message: "No data to update" })
+            return
+        }
+
+        if (!userId) {
+            res.status(400).json({ message: "User not found!" })
+            return
+        }
+
+        await user.updateOne({ _id: userId }, data)
+        res.status(201).json({ message: "User updated !" })
+    } catch (error) {
+        res.status(500).json({ message: error.message || "Error while creating user" })
+    }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
-import { createUser, getUsers } from "./controllers/user.controller.js";
+import { createUser, deleteUser, getUsers, updateUser } from "./controllers/user.controller.js";
 import { getProducts } from "./controllers/product.controller.js";
 import { connectDB } from "./config/connectDB.js";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
 connectDB();
 
 const PORT = 8000;
@@ -13,6 +15,8 @@ const PORT = 8000;
 app.get('/get-users', getUsers)
 app.post('/create-user', createUser)
 app.get('/get-products', getProducts)
+app.delete('/delete-user/:id', deleteUser)
+app.put('/update-user/:id', updateUser)
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
